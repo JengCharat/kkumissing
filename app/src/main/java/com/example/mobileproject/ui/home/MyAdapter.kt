4791e71154590b92ba41f80773fccd12ff1c8707
Nichagan-J <nichagan2004@gmail.com>
2025-03-09 18:17:18 +0700

    test

---
+++ app/src/main/java/com/example/mobileproject/ui/home/MyAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.mobileproject
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(val items: Array<String>, val imageId: Array<Int>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.model, parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.nameTextView?.text = items[position]
+        holder.imageIdView?.setImageResource(imageId[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        internal var nameTextView: TextView? = itemView.findViewById(R.id.nameTxt)
+        internal var imageIdView: ImageView? = itemView.findViewById(R.id.thumbnail)
+    }
+}
---
+++ app/src/main/java/com/example/mobileproject/ui/home/activity_about_us.kt
@@ -0,0 +1,21 @@
+package com.example.mobileproject.ui.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mobileproject.R
+
+class activity_about_us : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_about_us)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
