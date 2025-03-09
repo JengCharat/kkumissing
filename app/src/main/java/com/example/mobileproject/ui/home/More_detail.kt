09ac0e39fa75d208d12144e5834d6d6675b66c0a
JengCharat <zazajeng401@gmail.com>
2025-03-09 15:03:26 +0700

    feat/show_all_image_in_more_detail

--- app/src/main/java/com/example/mobileproject/ui/home/More_detail.kt
+++ app/src/main/java/com/example/mobileproject/ui/home/More_detail.kt
@@ -11,8 +11,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.mobileproject.R
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
