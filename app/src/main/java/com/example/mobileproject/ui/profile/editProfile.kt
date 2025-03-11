e07bd9bbb949da03a707c263cd7095f124ef3b68
natthaneto <natthanet.k@kkumail.com>
2025-03-12 00:16:15 +0700

    edit profile redius

--- app/src/main/java/com/example/mobileproject/ui/profile/editProfile.kt
+++ app/src/main/java/com/example/mobileproject/ui/profile/editProfile.kt
@@ -1,19 +1,30 @@
 package com.example.mobileproject.ui.profile
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import com.example.mobileproject.R
 import com.example.mobileproject.databinding.FragmentEditProfileBinding
+import com.google.android.material.imageview.ShapeableImageView
 
 
 class editProfile : Fragment() {
     private var _binding: FragmentEditProfileBinding? = null
     private val binding get() = _binding!!
 
+    private var isDefaultImage = true  // สถานะรูปภาพปัจจุบัน
+
+    // เปิด Gallery เพื่อเลือกภาพ
+    private val pickImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        uri?.let {
+            binding.imageView3.setImageURI(it) // ตั้งค่ารูปที่เลือก
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +36,10 @@ class editProfile : Fragment() {
             findNavController().navigate(R.id.action_to_profileFragment)
         }
 
+        // คลิกปุ่มเพื่อเลือกภาพจาก Gallery
+        binding.imageView3.setOnClickListener {
+            pickImage.launch("image/*") // เปิด Gallery
+        }
     return root
     }
     override fun onDestroyView() {
