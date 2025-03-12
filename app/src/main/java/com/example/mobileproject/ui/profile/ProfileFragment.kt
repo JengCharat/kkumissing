458c3147e97980824953665930cdad9e0321670c
JengCharat <zazajeng401@gmail.com>
2025-03-12 17:32:54 +0700

    feat/user_profile

--- app/src/main/java/com/example/mobileproject/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/mobileproject/ui/profile/ProfileFragment.kt
@@ -1,33 +1,57 @@
 package com.example.mobileproject.ui.profile
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Base64
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobileproject.R
 import com.example.mobileproject.databinding.FragmentProfileBinding
+import com.example.mobileproject.ui.dashboard.img1
+import com.example.mobileproject.ui.dashboard.img2
+import com.example.mobileproject.ui.dashboard.img3
+import com.example.mobileproject.ui.dashboard.img4
 import com.example.mobileproject.ui.home.MyAdapter
 import com.example.mobileproject.ui.home.User
 import com.example.mobileproject.ui.home.db_server_ip
 import com.example.mobileproject.ui.home.selectedItem
+import com.example.mobileproject.ui.notifications.contact
+import com.example.mobileproject.ui.notifications.fname_2
+import com.example.mobileproject.ui.notifications.item_name
+import com.example.mobileproject.ui.notifications.item_type
+import com.example.mobileproject.ui.notifications.latitude2
+import com.example.mobileproject.ui.notifications.lname_2
+import com.example.mobileproject.ui.notifications.longitude2
+import com.example.mobileproject.ui.notifications.lost_place
+import com.example.mobileproject.ui.notifications.more_detail
+import com.example.mobileproject.ui.notifications.telNumber
 import com.google.firebase.auth.FirebaseAuth
 import com.example.mobileproject.ui.profile.ResultActivity
 import com.example.mobileproject.ui.profile.LoginActivity
+import com.example.mobileproject.ui.home.HomeFragment
+import com.example.mobileproject.ui.home.Users
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
 import java.io.IOException
+import java.io.InputStream
 import java.net.Socket
 import java.net.URLEncoder
 var email2:String = ""
@@ -40,7 +64,23 @@ class ProfileFragment : Fragment() {
     private var mAuth: FirebaseAuth? = null
     private val TAG: String = "ProfileFragment"
     private var mainSignin: Button? = null
+    var profile_image:String = ""
+    val get_profile_Image = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val uri: Uri? = result.data?.data
+            uri?.let {
+                val img1_show: ImageView? = binding.profileImg
+                img1_show!!.setImageURI(it) // แสดงภาพที่เลือก
+                //val base64Image = encodeImageToBase64(it)
+                //ImageData.base64Image = encodeImageToBase64(it)
+                //println("Base64: $base64Image") // สามารถส่งค่า Base64 ไปยัง Server ได้
+                profile_image = encodeImageToBase64(it).toString()
 
+            }
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -61,12 +101,14 @@ class ProfileFragment : Fragment() {
             email2 = mAuth!!.currentUser?.email.toString()
             binding.mainLoginButton?.setText("logout")
             binding.gmail.setText("gmail: ${mAuth!!.currentUser?.email}")
-            get("select * from items where email = '${mAuth!!.currentUser?.email}'")
+            get("SELECT * from items  WHERE items.email = '${mAuth!!.currentUser?.email}'")
+
         }
         else{
             binding.mainLoginButton?.setText("login")
 
         }
+
         binding.profileReportButton.setOnClickListener {
             get("SELECT * FROM items WHERE email = '${mAuth!!.currentUser?.email}'  AND report_or_missing = 2 ORDER BY id DESC;")
         }
@@ -75,7 +117,14 @@ class ProfileFragment : Fragment() {
         }
 
 
+        binding.profileImg.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            get_profile_Image.launch(intent)
 
+        }
+        binding.updateProfile.setOnClickListener {
+            set_img()
+        }
         binding.mainLoginButton ?.setOnClickListener {
             if (mAuth!!.currentUser != null) {
                 mAuth!!.signOut()
@@ -98,6 +147,7 @@ class ProfileFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
     fun get(sqlCommand:String) {
 
         Thread {
@@ -200,7 +250,124 @@ class ProfileFragment : Fragment() {
 
 
                     recyclerView?.adapter = MyAdapter(itemList)
+                    fun decodeBase64ToBitmap_profile_image(): Bitmap? {
+                        return try {
+                            // val decodedBytes = Base64.decode(base64String, Base64.DEFAULT)
+                            val decodedBytes = Base64.decode(profile_image, Base64.DEFAULT)
+                            BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            null
+                        }
+                    }
 
+                    val profile_image_decode = decodeBase64ToBitmap_profile_image()
+                    binding.profileImg.setImageBitmap(profile_image_decode)
+
+
+                    get_profile_image("select * from users where users.email = '${mAuth!!.currentUser?.email}'")
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+        }.start()
+
+    }
+    fun get_profile_image(sqlCommand:String) {
+
+        Thread {
+            try {
+                // ตั้งค่าข้อมูลที่ต้องการส่ง
+                val host = db_server_ip
+                val path = "/myapi/test6.php"
+
+                //val sqlCommand = "INSERT INTO name (name, image) VALUES ('admin3', '12')"
+                //val sqlCommand = "select * from name where name = 'fan'"
+                val postData = "sql_command=" + URLEncoder.encode(sqlCommand, "UTF-8") // URL Encode แค่ค่า ไม่รวม key
+
+                // สร้าง HTTP Request แบบ Manual
+                val request = StringBuilder()
+                request.append("POST $path HTTP/1.1\r\n")
+                request.append("Host: $host\r\n")
+                request.append("Content-Type: application/x-www-form-urlencoded\r\n")
+                request.append("Content-Length: ${postData.toByteArray().size}\r\n")
+                request.append("Connection: close\r\n\r\n")
+                request.append(postData)
+
+                // แสดง HTTP Request ที่จะถูกส่งไป
+                println("Request:\n$request")
+
+                // สร้าง Socket ไปยังเซิร์ฟเวอร์
+                val socket = Socket(host, 80)
+                socket.soTimeout = 60000000
+                // ส่งข้อมูลไปยังเซิร์ฟเวอร์
+                val outputStream = socket.getOutputStream()
+                outputStream.write(request.toString().toByteArray())
+                outputStream.flush()
+// ใช้ BufferedInputStream เพื่อรองรับข้อมูลขนาดใหญ่
+                val inputStream = BufferedInputStream(socket.getInputStream())
+                val byteArray = ByteArray(32)
+                val responseBuilder = StringBuilder()
+
+                var bytesRead: Int
+                while (inputStream.read(byteArray).also { bytesRead = it } != -1) {
+                    responseBuilder.append(String(byteArray, 0, bytesRead))
+                }
+                /*
+                                // อ่าน Response จากเซิร์ฟเวอร์
+                                val inputStream = socket.getInputStream()
+                                val response = inputStream.bufferedReader().use { it.readText() }
+                                println("Server Response:\n$response")*/
+                // ปิดการเชื่อมต่อ
+                socket.close()
+                // อัปเดต UI บน Main Thread
+                activity?.runOnUiThread {
+                    //val responseBody = responseBuilder.toString().substringAfter("\r\n\r\n")
+                    val responseBody = responseBuilder.toString()
+                        .substringAfter("\r\n\r\n") // ตัด Header HTTP ออกก่อน
+                        .substringAfter("[")        // เริ่มนับที่ `[`
+                        .substringBeforeLast("]")   // จบที่ `]`
+                        .let { "[$it]" }            // ใส่ `[` และ `]` กลับไป
+
+                    val listType = object : TypeToken<List<Users>>() {}.type
+                    val users: List<Users> = Gson().fromJson(responseBody, listType)
+                    if (users.isNullOrEmpty()) {
+                        Toast.makeText(context, "not found", Toast.LENGTH_SHORT).show()
+                        return@runOnUiThread
+                    }
+
+                    ///////////////////////////
+                    val itemList = listOf(users)
+                    ///////////////////////////
+
+
+                    //nameTest.text = responseBody
+                    // println(responseBody)
+                    //var name_test:TextView? = null
+                    //name_test = findViewById(R.id.name_test)
+
+                    //name_test!!.text = users[0].name
+                    println("get IMGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG123")
+                    val x = Base64.decode(users[0].profile, Base64.DEFAULT)
+                    println(x)
+                    //println(users[0].image)
+                    // fun decodeBase64ToBitmap(base64String: String): Bitmap? {
+                    fun decodeBase64ToBitmap(): Bitmap? {
+                        return try {
+                            // val decodedBytes = Base64.decode(base64String, Base64.DEFAULT)
+                            val decodedBytes = Base64.decode(x, Base64.DEFAULT)
+                            BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            null
+                        }
+                    }
+
+
+
+                    val profile_image_decode = decodeBase64ToBitmap()
+                    binding.profileImg.setImageBitmap(profile_image_decode)
 
 
 
@@ -212,5 +379,50 @@ class ProfileFragment : Fragment() {
         }.start()
 
     }
+    fun set_img() {
+        //println("encode imageeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+        //println(ImageData.base64Image)
+        Thread {
+            try {
+                val postData = "sql_command=" + URLEncoder.encode("UPDATE users SET profile = '$profile_image' WHERE email = '${mAuth!!.currentUser?.email}'", "UTF-8"
+                ).trim()
+                val host = db_server_ip
+                val path = "/myapi/test5.php"
+                // สร้าง HTTP Request แบบ Manual
+                val request = StringBuilder()
+                request.append("POST $path HTTP/1.1\r\n")
+                request.append("Host: $host\r\n")
+                request.append("Content-Type: application/x-www-form-urlencoded\r\n")
+                request.append("Content-Length: ${postData.toByteArray().size}\r\n")
+                request.append("Connection: close\r\n\r\n")
+                request.append(postData)
+                // แสดง HTTP Request ที่จะถูกส่งไป
+                println("Request:\n$request")
+                // สร้าง Socket ไปยังเซิร์ฟเวอร์
+                val socket = Socket(host, 80)
+                socket.soTimeout = 60000000
+                // ส่งข้อมูลไปยังเซิร์ฟเวอร์
+                val outputStream = socket.getOutputStream()
+                outputStream.write(request.toString().toByteArray())
+                outputStream.flush()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
 
+        }.start()
+
+    }
+    private fun encodeImageToBase64(imageUri: Uri): String? {
+        return try {
+            val inputStream: InputStream? = requireContext().contentResolver.openInputStream(imageUri)
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            val outputStream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            val byteArray: ByteArray = outputStream.toByteArray()
+            Base64.encodeToString(byteArray, Base64.DEFAULT)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
 }
