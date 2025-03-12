deb8209c4d6874988dc3a7ad732a9d33dcc457cd
natthaneto <natthanet.k@kkumail.com>
2025-03-12 18:49:21 +0700

    fix/profile_not_update

--- app/src/main/java/com/example/mobileproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mobileproject/ui/home/HomeFragment.kt
@@ -14,6 +14,7 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobileproject.R
@@ -129,6 +130,7 @@ class HomeFragment : Fragment() {
 //        homeViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
+
 
         return root
     }
